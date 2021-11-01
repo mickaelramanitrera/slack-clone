1ab4c1b8fed50bf39a06c1aedceb5169aac6f30e
Mickaël RAMANITRERA <mickael@wau.travel>
2021-11-01 08:52:16 +0300

    Handle JWT auth for graphql client

--- frontend-react/src/providers/index.tsx
+++ frontend-react/src/providers/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { reactLocalStorage } from 'reactjs-localstorage';
 // theme
 import ThemeConfig from '../theme';
 import GlobalStyles from '../theme/globalStyles';
@@ -11,9 +13,27 @@ import store from '../redux/store';
 // Providers
 import { SnackbarProvider } from './Snackbar';
 import UserConnectedProvider from './UserConnected';
+import type { IUser } from '../types/user';
+
+const graphqlLink = createHttpLink({
+  uri: process.env.REACT_APP_BACKEND_GRAPHQL || '',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const { jwt } = reactLocalStorage.getObject('user') as IUser;
+  const jwtIsValid = jwt !== undefined && typeof jwt === 'string';
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: jwtIsValid ? `Bearer ${jwt}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_BACKEND_GRAPHQL || '',
+  link: authLink.concat(graphqlLink),
   cache: new InMemoryCache(),
 });
 
